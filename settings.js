0d5b4ad546b80031be7785793d24ac287f6bd342
kavisakaveesha-debug <kavisakaveesha@gmail.com>
2025-09-20 08:44:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-module.exports = {
+module.exports = { @@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') { 
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=1913448104674437' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
